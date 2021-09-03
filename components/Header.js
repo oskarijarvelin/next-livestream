330b9503a79f4eb3835c106be625b3d0449ce3a3
Oskari Järvelin <contact@oskarijarvelin.fi>
2021-09-03 11:06:22 +0300

    Removed login, added avidly

--- components/Header.js
+++ components/Header.js
@@ -3,22 +3,22 @@ import NextLink from 'next/link'
 
 export default function Header() {
     return (
-        <Flex bg="bg" color="body" borderBottom="1px solid #DEDEDE">
-            <Wrap align="center" px={{base: "4", lg: "8"}} py="4">
-                <WrapItem py={{base: "2", md: "2", lg: "4", xl:"8"}}>
-                    <Heading as="p" fontSize={{base: "16px", sm: "20px", md: "24px", lg: "32px", xl: "36px"}}>
-                        <NextLink href="/" passHref>
-                            <Link _hover={{textDecoration: "none"}}>
-                                Viestintäpäivä Plussa
-                            </Link>
-                        </NextLink>
-                    </Heading>
-                </WrapItem>
-            </Wrap>
-            <Spacer />
-            <Box px={{base: "4", lg: "8"}} py="4">
-                <Text size="xs" fontSize="14px" fontWeight="600" mb="1rem" opacity="60%">Yhteistyökumppanit</Text>
-                <Image src="stl.svg" h="60px" maxH="60px" alt="Suomen tiedetoimittajain liiton logo" />
+        <Flex bg="bg" color="body" borderBottom="1px solid #DEDEDE" alignItems="center" justifyContent={{base: "flex-start", md: "space-between"}} grow={{base: 0, md: 1}} wrap="wrap">
+            <Box align="center" borderBottom={{base: "1px solid #DEDEDE", md: "none"}} px={{base: "4", lg: "8"}} py="4" flex={{base: "100%", md: "auto"}}>
+                <Heading as="p" fontSize={{base: "24px", sm: "28px", lg: "32px", xl: "36px"}} textAlign={{base: "center", md: "left"}}>
+                    <NextLink href="/" passHref>
+                        <Link _hover={{textDecoration: "none"}}>
+                            Viestintäpäivä Plussa
+                        </Link>
+                    </NextLink>
+                </Heading>
+            </Box>
+            <Box pl="4" py="4" flex={{base: "100%", md: "unset"}}>
+                <Text size="xs" fontSize="14px" fontWeight="600" opacity="60%" textAlign={{base: "center", md: "left"}}>Yhteistyökumppanit</Text>
+                <Flex alignItems="center" justifyContent={{base: "center", md: "flex-start"}}>
+                    <Image src="stl.svg" h="60px" maxH="60px" maxW="50%" alt="Suomen tiedetoimittajain liiton logo" />
+                    <Image src="avidly.svg" h="80px" maxH="80px" maxW="50%" alt="Avidlyn logo" />
+                </Flex>
             </Box>
         </Flex>
   );
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -10,21 +10,22 @@ export default function LoginForm({adminOnly}) {
     const handleClick = () => setShow(!show)
     const loginUser = event => {
         event.preventDefault() 
-        let shaEmail = sha1(event.target.email.value.toLowerCase()).toLowerCase().split("").reverse().join("")
-        if ( shaEmail == event.target.password.value.toLowerCase() ) {
-            if ( adminOnly ) {
-                let admins = process.env.NEXT_PUBLIC_ADMINS.split(",")
-                if ( admins.includes(shaEmail) ) {
-                    setLogin(true)
-                } else {
-                    alert("Sinulla ei ole riittäviä oikeuksia.")
-                }
-            } else {
-                setLogin(true)
-            }
-        } else {
-            alert("Salasanasi on väärä. Yritä uudelleen.")
-        }
+        setLogin(true)
+        //let shaEmail = sha1(event.target.email.value.toLowerCase()).toLowerCase().split("").reverse().join("")
+        //if ( shaEmail == event.target.password.value.toLowerCase() ) {
+        //    if ( adminOnly ) {
+        //        let admins = process.env.NEXT_PUBLIC_ADMINS.split(",")
+        //        if ( admins.includes(shaEmail) ) {
+        //            setLogin(true)
+        //        } else {
+        //            alert("Sinulla ei ole riittäviä oikeuksia.")
+        //        }
+        //    } else {
+        //        setLogin(true)
+        //    }
+        //} else {
+        //    alert("Salasanasi on väärä. Yritä uudelleen.")
+        //}
 
     }
   
@@ -35,17 +36,6 @@ export default function LoginForm({adminOnly}) {
                     <InputGroup>
                         <InputLeftElement pointerEvents="none" children={<Icon as={HiOutlineMail} color="gray.500"/>} />
                         <Input type="email" placeholder="Sähköpostiosoitteesi" />
-                    </InputGroup>
-                </FormControl>
-                <FormControl id="password" name="password">
-                    <InputGroup size="md">
-                        <InputLeftElement pointerEvents="none" children={<Icon as={HiOutlineLockClosed} color="gray.500"/>} />
-                        <Input pr="4.5rem" type={show ? "text" : "password"} placeholder="Salasanasi" />
-                        <InputRightElement width="4.5rem">
-                            <Button bg="current" color="body" h="1.75rem" size="sm" onClick={handleClick}>
-                                {show ? "Piilota" : "Näytä"}
-                            </Button>
-                        </InputRightElement>
                     </InputGroup>
                 </FormControl>
                 <Button type="submit" bg="current" color="body" size="md">
